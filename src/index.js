4c202ddf00634c3e419d9d5818d006db46918b92
Antheny Wells <anthenywells@Go-away.lan>
2019-04-09 15:48:00 -0400

    Loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay"
+import Loader from './Loader';
 
 class App extends React.Component {
   state = {lat: null, errorMessage: ''};
@@ -13,8 +14,6 @@ class App extends React.Component {
     );
   }
 
-
-
   render() {
     if(this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
@@ -23,7 +22,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat}/>
     }
 
-    return <div>Loading</div>
+    return <Loader message="Please accept location request"/>
   }
 }
 
