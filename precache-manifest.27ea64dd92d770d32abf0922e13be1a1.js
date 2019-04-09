9484dfe3dbe09c77dd417ae178b1097478e650b4
Antheny Wells <anthenywells@Go-away.lan>
2019-04-09 16:21:52 -0400

    Updates

---
+++ precache-manifest.27ea64dd92d770d32abf0922e13be1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dc1f5fb718226aca7f3",
+    "url": "/simple-season-finder/static/js/runtime~main.8ed748f1.js"
+  },
+  {
+    "revision": "adf7426f0c19264e52f6",
+    "url": "/simple-season-finder/static/js/main.416833a6.chunk.js"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/simple-season-finder/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "adf7426f0c19264e52f6",
+    "url": "/simple-season-finder/static/css/main.3cf52abb.chunk.css"
+  },
+  {
+    "revision": "de352dfd1b9affc7f144cc7c39e3842f",
+    "url": "/simple-season-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/simple-season-finder/precache-manifest.27ea64dd92d770d32abf0922e13be1a1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/simple-season-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
