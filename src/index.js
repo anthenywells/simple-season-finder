924236fda81ca7b396b62bce6a76d33bfef97b19
Antheny Wells <anthenywells@Go-away.lan>
2019-04-09 16:12:33 -0400

    index refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay"
-import Loader from './Loader';
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
-  state = {lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage: "" };
 
   // initial data loading (such as api request)
   componentDidMount() {
@@ -13,16 +13,19 @@ class App extends React.Component {
       err => this.setState({ errorMessage: err.message })
     );
   }
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Loader message="Please accept location request" />;
+  }
 
   render() {
-    if(this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-    if(this.state.lat && !this.state.errorMessage) {
-      return <SeasonDisplay lat={this.state.lat}/>
-    }
-
-    return <Loader message="Please accept location request"/>
+    return <div>{this.renderContent()}</div>;
   }
 }
 
